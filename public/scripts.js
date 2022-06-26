3fc78c06a17a31d7e9cc36176203f863da29dfec
Robson Sampaio <49698865+Robson-SA@users.noreply.github.com>
2022-06-26 19:09:15 -0300

    add public

---
+++ public/scripts.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
